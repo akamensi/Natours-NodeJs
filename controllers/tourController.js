aa78b1e3366e98b2e3a993220edd76aae07ecd4d
akamensi <akez.muhammed@gmail.com>
2024-09-18 19:57:26 +0200

    Anathor Way To Create Document

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,77 +1,69 @@
-const fs = require('fs');
+/* const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
+); */
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+const Tour = require('../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'name and price are required',
-    });
-  }
-  next();
-};
+// exports.checkBody = (req, res, next) => {
+//   if (!req.body.name || !req.body.price) {
+//     return res.status(404).json({
+//       status: 'fail',
+//       message: 'name and price are required',
+//     });
+//   }
+//   next();
+// };
 
-exports.checkID = (req, res, next, val) => {
-  console.log(`id = ${val}`);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-};
+// exports.checkID = (req, res, next, val) => {
+//   console.log(`id = ${val}`);
+//   // if (req.params.id * 1 > tours.length) {
+//   //   return res.status(400).json({
+//   //     status: 'fail',
+//   //     message: 'Invalid ID',
+//   //   });
+//   // }
+//   next();
+// };
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    results: tours.length,
-    data: { tours },
+    // results: tours.length,
+    // data: { tours },
   });
 };
 
 exports.getOneTour = (req, res) => {
   //console.log(req.params)
 
-  const id = req.params.id * 1; //change into a number
+  // const id = req.params.id * 1; //change into a number
 
-  const tour = tours.find((el) => el.id === id);
+  // const tour = tours.find((el) => el.id === id);
 
   res.status(200).json({
     status: 'success',
-    data: { tour },
+    // data: { tour },
   });
 };
 
-exports.createTour = (req, res) => {
-  //console.log(req.body);
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({})
+    // newTour.save().then()
 
-  // Handle cases where tours array is empty
-  const newId = tours.length > 0 ? tours[tours.length - 1].id + 1 : 1;
+    const newTour = await Tour.create(req.body);
 
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      if (err) {
-        return res.status(500).json({
-          status: 'fail',
-          message: 'Internal Server Error',
-        });
-      }
-      res.status(201).json({
-        status: 'success',
-        data: { tour: newTour },
-      });
-    }
-  );
+    res.status(201).json({
+      status: 'success',
+      data: { tour: newTour },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'Fail',
+      message: 'Invalid data sent!',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
