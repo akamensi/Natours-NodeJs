895be949e065d3dc381a656506cf1b41dba9d61b
akamensi <akez.muhammed@gmail.com>
2024-09-18 18:12:22 +0200

    Creating Model and Document Tour

--- server.js
+++ server.js
@@ -19,6 +19,30 @@ mongoose
   })
   .then(() => console.log('DB is connected'));
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'A tour must have a price'] },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The New tour',
+  price: 400,
+});
+
+testTour
+  .save()
+  .then((doc) => console.log(doc))
+  .catch((err) => {
+    console.log('Error bom:', err);
+  });
+
 const port = process.env.PORT;
 
 app.listen(port, () => {
